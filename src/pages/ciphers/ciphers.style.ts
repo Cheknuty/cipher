e107e51efd461ca33ad3b1e9d1012c2cd1aa35c4
ХИКМАТ Азизов <azizovxikmatjon04@gmail.com>
2022-10-06 11:33:26 +0500

    added redirects

--- src/pages/ciphers/ciphers.style.ts
+++ src/pages/ciphers/ciphers.style.ts
@@ -3,3 +3,11 @@ import styled from "styled-components";
 export const CiphersWrapper = styled.main`
 
 `
+
+export const CiphersList = styled.ul`
+
+`
+
+export const CiphersItem = styled.li`
+
+`
--- src/pages/ciphers/ciphers.tsx
+++ src/pages/ciphers/ciphers.tsx
@@ -1,7 +1,25 @@
-import { CiphersWrapper } from "./ciphers.style";
+import { Container } from "../../common/container/container";
+import { CiphersItem, CiphersList, CiphersWrapper } from "./ciphers.style";
 
 export function Ciphers() {
     return (
-        <CiphersWrapper>ciphers</CiphersWrapper>
+        <CiphersWrapper>
+            <Container>
+                <CiphersList>
+                    <CiphersItem>
+                        symmetric key algorithms
+                    </CiphersItem>
+                    <CiphersItem>
+                        asymmetric key algorithms
+                    </CiphersItem>
+                    <CiphersItem>
+                        block ciphers
+                    </CiphersItem>
+                    <CiphersItem>
+                        stream ciphers
+                    </CiphersItem>
+                </CiphersList>
+            </Container>
+        </CiphersWrapper>
     )
 }
